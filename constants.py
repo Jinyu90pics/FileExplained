73fcfeec4dcae7b03d4f0a189ef18bb0d66710eb
Jinyu90pics <130891970+Jinyu90pics@users.noreply.github.com>
2025-01-02 09:52:27 +0800

    Update constants.py

--- constants.py
+++ constants.py
@@ -3,4 +3,4 @@ from dotenv import load_dotenv
 
 load_dotenv(".env")
 
-OPENAI_KEY = os.getenv('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')
+OPENAI_KEY = os.getenv('OPENAI_KEY')
